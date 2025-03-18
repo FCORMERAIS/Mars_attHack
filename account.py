25aee27164ee987161d3175b53948923087a5600
Your Name <you@example.com>
2025-03-18 10:41:10 +0100

    Nettoyage de la page account

--- account.py
+++ account.py
@@ -2,13 +2,12 @@ import streamlit as st
 
 def show_account():
     st.title("Compte")
-    sommeil = st.time_input("Entrez votre nombre d'heures de sommeil")
-    sport =  st.number_input("Saisissez le nombre de séance de sport à réalisé par semaine")
-    travail =  st.number_input("Saisissez le nombre d'heures de travail par semaine")
-    pause =  st.number_input("Saisissez le temps nécessaire de votre pause")
-    transport =  st.number_input("Saisissez le temps moyen passé dans les transport")
-    Divertisseement =  st.text_input("Saisissez le temps moyen passé à vous divertir")
-
+    sommeil = st.time_input("Heures de sommeil:")
+    sport =  st.number_input("Séance de sport à réalisé:")
+    travail =  st.number_input("Heures de travail:")
+    pause =  st.number_input("Temps de pause nécessaire:")
+    transport =  st.number_input("Durée moyenne passé dans les transport:")
+    divertissement =  st.text_input("Temps passé à vous divertir")
 
 if __name__ == "__main__":
     show_account()
